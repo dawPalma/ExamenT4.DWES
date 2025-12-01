30e828453b457d52bafd2ad0cba8fc5a0d402426
Jose <jpallop0304@g.educaand.es>
2025-12-01 18:14:15 +0100

    Ordenar

---
+++ src/components/boton.jsx
@@ -0,0 +1,10 @@
+
+function Boton() {
+    return (
+        <button type="submit" className="p-2 rounded-md bg-blue-500 text-white">
+            Submit
+        </button>
+    )
+}
+
+export default Boton
