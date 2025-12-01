926c6a913475bd9b653ee2215c90a4cd6c50f816
Jose <jpallop0304@g.educaand.es>
2025-12-01 17:28:36 +0100

    SQL/JSON

--- src/data/db.sql
+++ src/data/db.sql
@@ -4,37 +4,6 @@ CREATE DATABASE biblioteca;
 USE biblioteca;
 
 
-CREATE TABLE alumnos (
-    id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-    nombre VARCHAR(200) NOT NULL,
-    localidad VARCHAR(200),
-    fecha_nacimiento DATE,
-    createdAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    updatedAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
-);
-
-INSERT INTO alumnos (nombre, localidad, fecha_nacimiento) 
-VALUES 
-  ('José', 'Montilla', '2000-01-01'),
-  ('Juan', 'Lucena', '2000-01-01'),
-  ('Ana', 'Lucena', '2000-01-01');
-
-
-CREATE TABLE profesores (
-    id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-    nombre VARCHAR(200) NOT NULL,
-    especialidad VARCHAR(200),
-    estado_civil VARCHAR(200),
-    createdAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    updatedAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
-);
-
-INSERT INTO profesores (nombre, especialidad, estado_civil) 
-VALUES 
-  ('José Profe', 'Matemáticas', 'Casado'),
-  ('Juan Profe', 'Lengua', 'Casado'),
-  ('Ana Profe', 'Inglés', 'Casado');
-
 CREATE TABLE autores (
     id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
     nombre VARCHAR(200) NOT NULL,
